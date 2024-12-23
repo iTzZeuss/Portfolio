2075db04da8743bb4dbcf08229032127765cdd1a
iTzZeuss <blackangelmusic22@gmail.com>
2024-12-23 19:27:53 +0200

    animation worksss

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,10 +1,34 @@
 import { animate, motion } from "framer-motion";
 import React from "react";
 
+const textSlide = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      duration: 20,
+    },
+  },
+};
+
 function Clock() {
   return (
-    <div className="bg-gray-200 absolute top-[900px] w-full h-[500px] justify-items-center content-center grid gap-3">
-      <div
+    <div
+      className="bg-gray-200 absolute top-[900px] w-full h-[500px] 
+                 justify-items-center content-center grid gap-3"
+    >
+      <motion.p
+        className="w-full font-bold text-7xl absolute top-2 left-3 whitespace-nowrap text-gray-600 opacity-75"
+        variants={textSlide}
+        initial="initial"
+        animate="animate"
+      >
+        John Doe Coder Content Creator Experienced
+      </motion.p>
+      <motion.div
         className="size-36 bg-black"
         animate={{ rotate: "180deg" }}
         transition={{
@@ -12,7 +36,7 @@ function Clock() {
           ease: "backInOut",
         }}
         initial={{ rotate: "0deg" }}
-      ></div>
+      ></motion.div>
     </div>
   );
 }
